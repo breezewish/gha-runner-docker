ad6314ce0feed31deba073425b5e286879ff6671
Wish <breezewish@outlook.com>
2024-07-18 17:36:45 +0800

    troubleshoot
    Signed-off-by: Wish <breezewish@outlook.com>

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 
 set -euo pipefail
 
-docker buildx build -t gha-runner-base -f Dockerfile.gha-runner-base .
-docker buildx build -t gha-runner -f Dockerfile.gha-runner .
-docker buildx build -t gha-runner-tiflash -f Dockerfile.gha-runner-tiflash .
+docker buildx build --progress=plain -t gha-runner-base -f Dockerfile.gha-runner-base .
+docker buildx build --progress=plain -t gha-runner -f Dockerfile.gha-runner .
+docker buildx build --progress=plain -t gha-runner-tiflash -f Dockerfile.gha-runner-tiflash .
 
 docker image tag gha-runner         hub.pingcap.net/sunxiaoguang/serverless/gha-runner:latest
 docker image tag gha-runner-tiflash hub.pingcap.net/sunxiaoguang/serverless/gha-runner:tiflash-latest
