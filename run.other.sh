16ea8f991910f9f21fd282c8647cc3c9e1cde5c9
Wish <breezewish@outlook.com>
2024-07-18 14:06:54 +0800

    grumble
    Signed-off-by: Wish <breezewish@outlook.com>

--- run.other.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-echo "Github Runner for $(hostname)"
-
-docker buildx build -t gha-runner .
-docker run -ti --name=gha-runner-$1-$2 -d --restart=always -v /var/run/docker.sock:/var/run/docker.sock gha-runner $1 $2 $3 "$(hostname)"
--- run.sh
+++ run.sh
@@ -7,8 +7,25 @@ if [ $# -lt 2 ]; then
     exit 1
 fi
 
+echo "Github Runner for $(hostname)"
+
+docker buildx build -t gha-runner-base -f Dockerfile.gha-runner-base .
+docker buildx build -t gha-runner -f Dockerfile.gha-runner .
+
 if [[ $2 == "tiflash-cse" ]]; then
-    ./run.tiflash.sh "$@"
+
+    docker buildx build -t gha-runner-tiflash -f Dockerfile.gha-runner-tiflash .
+    docker run -ti --name=gha-runner-$1-$2 -d --restart=always \
+        -v /var/run/docker.sock:/var/run/docker.sock \
+        -v tiflash_cargo_registry:/home/docker/.cargo/registry \
+        -v tiflash_cargo_git:/home/docker/.cargo/git \
+        -v tiflash_rustup:/home/docker/.rustup \
+        -v tiflash_ccache:/home/docker/.cache/ccache \
+        -v tiflash_git:/home/docker/actions-runner/_work/tiflash-cse \
+        gha-runner-tiflash $1 $2 $3 "$(hostname)"
+
 else
-    ./run.other.sh "$@"
+
+    docker run -ti --name=gha-runner-$1-$2 -d --restart=always -v /var/run/docker.sock:/var/run/docker.sock gha-runner $1 $2 $3 "$(hostname)"
+
 fi
