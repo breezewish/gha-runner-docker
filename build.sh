86fabce1c6748db4bfed14cbdfd5197d417846b4
Wish <breezewish@outlook.com>
2024-07-22 08:33:19 +0800

    push
    Signed-off-by: Wish <breezewish@outlook.com>

--- build.sh
+++ build.sh
@@ -11,5 +11,5 @@ docker buildx build --progress=plain -t gha-runner-tiflash --load -f Dockerfile.
 docker image tag gha-runner         hub.pingcap.net/sunxiaoguang/serverless/gha-runner:$ARCH-latest
 docker image tag gha-runner-tiflash hub.pingcap.net/sunxiaoguang/serverless/gha-runner:$ARCH-tiflash-latest
 
-# docker push hub.pingcap.net/sunxiaoguang/serverless/gha-runner:$ARCH-latest
-# docker push hub.pingcap.net/sunxiaoguang/serverless/gha-runner:$ARCH-tiflash-latest
+docker push hub.pingcap.net/sunxiaoguang/serverless/gha-runner:$ARCH-latest
+docker push hub.pingcap.net/sunxiaoguang/serverless/gha-runner:$ARCH-tiflash-latest
